5bf12dc995759b3b0497201e48ea414dfdfb6013
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-13 01:57:04 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@ def speak_to_grandma(text)
   if text == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif text == text.upcase
-    return "NO, NOT SINCE 1938!" 
+    return "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
